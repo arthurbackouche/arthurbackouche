618c4c8b6e019368018fc49464289c7b66e1dc6f
Arthur Backouche <arthurbackouche@Arthurs-MacBook-Pro.local>
2023-01-28 18:44:41 +1100

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/arthurbackouche/arthurbackouche/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/arthurbackouche/arthurbackouche/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/arthurbackouche/arthurbackouche/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/arthurbackouche/arthurbackouche/src/css/custom.css'),
+];
